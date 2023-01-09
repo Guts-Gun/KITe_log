3df0bbcb278976a481267cd0130a0c1e5226ed23
YunJae Park <zerglisk123@naver.com>
2023-01-10 00:13:36 +0900

    create

---
+++ src/main/java/gutsandgun/kite_log/KiTeLogApplication.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiTeLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiTeLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gutsandgun/kite_log/KiTeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiTeLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
