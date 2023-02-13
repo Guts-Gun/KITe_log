0bb482cef84d0001a0a6d3b736b5799f1e16ffe1
YunJae Park <zerglisk123@naver.com>
2023-02-13 15:33:07 +0900

    [TEST] 로그 수집 테스트 ver0.12

--- src/main/java/gutsandgun/kite_log/controller/LogController.java
+++ src/main/java/gutsandgun/kite_log/controller/LogController.java
@@ -3,17 +3,13 @@ package gutsandgun.kite_log.controller;
 import gutsandgun.kite_log.service.Logging;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("log")
 public class LogController {
